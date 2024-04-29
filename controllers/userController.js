66b1599942d7ba2488c092b076b244601b9c474d
Vr <vr@MacBook-Pro-de-Vr.local>
2024-04-29 16:19:01 +0200

    test

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,19 @@ const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const expressAsyncHandler = require('express-async-handler');
+const nodemailer = require('nodemailer');
+
+// Créez un transporteur nodemailer
+let transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: false, // true pour le port 465, false pour les autres ports
+  auth: {
+    user: process.env.SMTP_MAIL,
+    pass: process.env.SMTP_PASSWORD,
+  },
+});
+
 
 // Fonction pour inscrire un nouvel utilisateur
 exports.signupUser = expressAsyncHandler(async (req, res) => {
@@ -25,8 +38,19 @@ exports.signupUser = expressAsyncHandler(async (req, res) => {
   });
 
   if (user) {
+
+     // Envoyer un e-mail de confirmation
+     const mailOptions = {
+      from: process.env.SMTP_MAIL,
+      to: email,
+      subject: 'Confirmation d\'inscription',
+      text: `Bonjour ${name}, vous êtes maintenant inscrit sur notre plateforme.`,
+    };
+
+    await transporter.sendMail(mailOptions);
+
     const token = jwt.sign(
-      { id: user._id, email: user.email },
+      {_id: user._id, email: user.email },
       process.env.JWT_SECRET,
       { expiresIn: '24h' }
     );
@@ -43,21 +67,22 @@ exports.signupUser = expressAsyncHandler(async (req, res) => {
 
 // Fonction pour connecter un utilisateur existant
 exports.signinUser = expressAsyncHandler(async (req, res) => {
-  const { email, password } = req.body;
-
-  // Trouver l'utilisateur par email
+  const { email, name, password } = req.body;
+  console.log('Attempting to find user:', email);  // Log pour le débogage
   const user = await User.findOne({ email });
   if (!user) {
+    console.log('No user found with that email');  // Log pour le débogage
     return res.status(401).json({ message: 'Email ou mot de passe incorrect' });
   }
 
-  // Comparer le mot de passe haché avec le mot de passe fourni
+  console.log('User found, comparing password...');  // Log pour le débogage
   const isMatch = await bcrypt.compare(password, user.password);
   if (!isMatch) {
+    console.log('Password does not match');  // Log pour le débogage
     return res.status(401).json({ message: 'Email ou mot de passe incorrect' });
   }
 
-  // Créer un token si la connexion est réussie
+  console.log('Password matches, signing token...');  // Log pour le débogage
   const token = jwt.sign(
     { id: user._id, email: user.email },
     process.env.JWT_SECRET,
@@ -72,6 +97,7 @@ exports.signinUser = expressAsyncHandler(async (req, res) => {
   });
 });
 
+
 // Fonction pour obtenir les informations d'un utilisateur par son ID
 exports.getUser = expressAsyncHandler(async (req, res) => {
   const user = await User.findById(req.params.id);
@@ -80,11 +106,57 @@ exports.getUser = expressAsyncHandler(async (req, res) => {
     return;
   }
   res.json({
-    _id: user._id,
+    id: user._id,
     email: user.email,
     name: user.name
   });
 });
+
+exports.sendResetEmail = expressAsyncHandler(async (req, res) => {
+  const { email } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(404).json({ message: 'Utilisateur non trouvé' });
+  }
+  const resetToken = jwt.sign({ _id: user._id }, process.env.JWT_RESET_SECRET, { expiresIn: '15m' });
+  user.resetPasswordToken = resetToken;
+  user.resetPasswordExpire = Date.now() + 15 * 60 * 1000; // 15 minutes
+  await user.save();
+
+  // Construisez un lien de réinitialisation qui pointe vers l'interface frontend
+  const resetLink = `http://yourfrontenddomain.com/reset-password?token=${resetToken}`;
+  const mailOptions = {
+    from: process.env.SMTP_MAIL,
+    to: email,
+    subject: 'Réinitialisation de votre mot de passe',
+    text: `Bonjour, vous avez demandé la réinitialisation de votre mot de passe. Veuillez cliquer sur le lien suivant pour réinitialiser votre mot de passe: ${resetLink}`
+  };
+  await transporter.sendMail(mailOptions);
+  res.json({ message: 'Un e-mail de réinitialisation a été envoyé.' });
+});
+
+
+exports.resetPassword = expressAsyncHandler(async (req, res) => {
+  const { token, newPassword } = req.body;
+  const decoded = jwt.verify(token, process.env.JWT_RESET_SECRET);
+  
+  const user = await User.findOne({ _id: decoded._id, resetPasswordToken: token, resetPasswordExpire: { $gt: Date.now() } });
+  if (!user) {
+    return res.status(400).json({ message: 'Token invalide ou expiré' });
+  }
+
+  // Hasher le nouveau mot de passe avant de le sauvegarder
+  user.password = await bcrypt.hash(newPassword, 12);
+  user.resetPasswordToken = undefined;
+  user.resetPasswordExpire = undefined;
+  await user.save();
+
+  res.json({ message: 'Mot de passe réinitialisé avec succès.' });
+});
+
+
+
+
 
 // Fonction pour supprimer un utilisateur
 exports.deleteUser = expressAsyncHandler(async (req, res) => {
