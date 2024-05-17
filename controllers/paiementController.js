8e90993721660445c593ae0837343dcba3492b5b
Victor Barreyre <barreyrevictor.contact@gmail.com>
2024-05-17 18:00:39 +0200

    createcheckout

--- controllers/paiementController.js
+++ controllers/paiementController.js
@@ -1,62 +1,60 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const Facture = require('../models/Facture'); // Assurez-vous que le chemin vers votre modèle est correct
+const stripe = require('stripe')(process.env.STRIPE_SECRET_TOKEN);
 
-const createPaymentIntent = async (req, res) => {
+console.log("Stripe Secret Token:", process.env.STRIPE_SECRET_TOKEN);
+
+
+exports.createCheckoutSession = async (req, res) => {
   try {
-    const { amount, currency, emetteur, destinataire } = req.body;
-    console.log("Reçu pour le paiement :", amount, currency, emetteur, destinataire);
+    const { amount, currency, emetteur } = req.body; // Vérifiez que ces valeurs sont bien passées dans le corps de la requête
+    console.log("Creating checkout session with amount:", amount, "currency:", currency, "issuer:", emetteur);
 
-    // Parse JSON objects from strings if necessary
-    const parsedEmetteur = JSON.parse(emetteur);
-    const parsedDestinataire = JSON.parse(destinataire);
-
-    // Créer un Customer sur Stripe
-    const customer = await stripe.customers.create({
-      email: parsedEmetteur.email,
-      name: parsedEmetteur.name,
-      description: `Customer for ${parsedEmetteur.name}`,
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: [
+        {
+          price_data: {
+            currency: currency,
+            product_data: {
+              name: 'Facture',
+            },
+            unit_amount: amount,
+          },
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: `${req.headers.origin}/payment-success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${req.headers.origin}/payment-error`,
+      automatic_tax: { enabled: true },
     });
 
-    // Créer un PaymentMethod de type SEPA Debit
-    const paymentMethod = await stripe.paymentMethods.create({
-      type: 'sepa_debit',
-      sepa_debit: { iban: parsedEmetteur.iban },
-      billing_details: {
-        name: parsedEmetteur.name,
-        email: parsedEmetteur.email,
-      },
-    });
-
-    // Attacher le PaymentMethod au Customer
-    await stripe.paymentMethods.attach(paymentMethod.id, {
-      customer: customer.id,
-    });
-
-    // Créer un PaymentIntent avec le PaymentMethod et spécifier 'sepa_debit' comme type autorisé
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: amount,
-      currency: currency || "eur",
-      customer: customer.id,
-      payment_method: paymentMethod.id,
-      off_session: true,
-      confirm: true,
-      payment_method_types: ['sepa_debit'], // Spécifier ici que sepa_debit est autorisé
-      metadata: {
-        emetteur: JSON.stringify(parsedEmetteur),
-        destinataire: JSON.stringify(parsedDestinataire),
-      },
-    });
-
-    // Renvoyer le clientSecret et l'ID de la facture au client pour permettre le paiement
-    res.send({
-      success: true,
-      message: 'PaymentIntent créé et facture sauvegardée avec succès.',
-      clientSecret: paymentIntent.client_secret,
-    });
+    console.log("Checkout Session created successfully", session.id);
+    res.send({ sessionId: session.id });
   } catch (error) {
-    console.error("Erreur lors de la création du PaymentIntent et de la facture:", error);
-    res.status(500).send({ success: false, message: error.message });
+    console.error("Failed to create checkout session:", error);
+    res.status(500).send({ error: error.message });
   }
 };
 
-module.exports = { createPaymentIntent };
+// Function to create a payment intent
+exports.createPaymentIntent = async (req, res) => {
+  try {
+    const { amount } = req.body; // Assurez-vous que le montant est en centimes et correctement passé dans le corps de la requête
+    console.log("Creating payment intent with amount:", amount);
+
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount,
+      currency: 'eur', // Devise
+      confirmation_method: 'automatic',
+      confirm: true,
+    });
+
+    console.log("Payment Intent created successfully", paymentIntent.id);
+    res.send({
+      clientSecret: paymentIntent.client_secret, // Envoyer le client secret pour finaliser le paiement côté client
+    });
+  } catch (error) {
+    console.error("Failed to create payment intent:", error);
+    res.status(500).send({ error: error.message });
+  }
+};
--- server.js
+++ server.js
@@ -3,30 +3,36 @@ const dotenv = require("dotenv");
 const emailRoutes = require("./routes/emailRoutes");
 const paiementRoutes = require('./routes/paiementRoutes');
 const mongoose = require('mongoose'); // Importez mongoose
-const cors = require('cors')
+const cors = require('cors');
 
 dotenv.config();
 
 const app = express();
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+app.use(cors({
+  origin: 'http://localhost:5173'  // Autoriser uniquement les requêtes de ce domaine
+}));
 app.use(express.static('public'));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 
 
 // Routes
 app.use("/email", emailRoutes);
 app.use("/paiement", paiementRoutes);
 
-
 app.get("/", (req, res) => {
   res.send("The Backend of my Invoice App");
 });
 
-// Utilisez mongoose pour connecter à MongoDB
 mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("Connected to MongoDB successfully"))
-.catch(err => console.error("Failed to connect to MongoDB", err));
+  .then(() => {
+    console.log("Connected to MongoDB successfully");
+  
+  })
+  .catch(err => console.error("Failed to connect to MongoDB", err));
+
 mongoose.set('debug', true);
 
 
